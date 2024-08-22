489966ab523c6b4976de0e6d4c0e9bcbab0486c9
lolmaosteffen <steffen-1997@live.no>
2024-08-22 11:28:59 +0200

    dsgfh

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
+console.log(blinkingLights)
 function changeColor(rødt, gult, grønnt) {
     document.getElementById("rødt").style.opacity = rødt;
     document.getElementById("gult").style.opacity = gult;
     document.getElementById("grønnt").style.opacity = grønnt;
 }
-console.log(changeColor)
