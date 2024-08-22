1dd3e9d4b1d9227ab57601b833b38b6396363f2d
lolmaosteffen <steffen-1997@live.no>
2024-08-22 10:55:15 +0200

    fant endeligt udav dritten

--- code.js
+++ code.js
@@ -1,20 +1,6 @@
-function makeItRed() {
-    document.getElementById("rødt").style.opacity = '1'
-    document.getElementById('gult').style.opacity = '0.2'
-    document.getElementById("grønnt").style.opacity = '0.2'
+function changeColor(rødt, gult, grønnt) {
+    document.getElementById("rødt").style.opacity = rødt;
+    document.getElementById("gult").style.opacity = gult;
+    document.getElementById("grønnt").style.opacity = grønnt;
 }
-function makeItRedAndYellow() {
-    document.getElementById("rødt").style.opacity = '1'
-    document.getElementById('gult').style.opacity = '1'
-    document.getElementById("grønnt").style.opacity = '0.2'
-}
-function avKnapp() {
-    document.getElementById("rødt").style.opacity = '0.2'
-    document.getElementById('gult').style.opacity = '0.2'
-    document.getElementById("grønnt").style.opacity = '0.2'
-}
-function makeItGreen() {
-    document.getElementById("grønnt").style.opacity = '1'
-    document.getElementById("rødt").style.opacity = '0.2'
-    document.getElementById('gult').style.opacity = '0.2'
-}
+console.log(changeColor)
